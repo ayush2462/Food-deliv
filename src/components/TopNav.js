c512bedfe6d0f3875ce7ae20486ba3120866e138
ayush2462 <128534675+ayush2462@users.noreply.github.com>
2023-12-23 16:06:07 +0530

    Changes

---
+++ src/components/TopNav.js
@@ -0,0 +1,11 @@
+
+
+const TopNav = () => {
+  return (
+    <div className="max-w-[1520] mx-auto flex justify-between items-center p-4">
+      hello
+    </div>
+  );
+};
+
+export default TopNav;
